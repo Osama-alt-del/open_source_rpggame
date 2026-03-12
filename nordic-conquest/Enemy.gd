21992fd73dc0ce876e9e131862972d992be1f1c5
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-12 12:30:58 +0900

    Item wip

---
+++ nordic-conquest/Enemy.gd
@@ -0,0 +1,12 @@
+class_name Enemy
+
+# This class should have x and y as it's coordinates
+extends GridItem
+
+#Different for barbarian and viking, so we will have this set as nothing
+var health
+var attack
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
---
+++ nordic-conquest/GridItem.gd
@@ -0,0 +1,19 @@
+extends Node
+# Node : standard 2d object library
+
+# Make a class:
+# with this, GODOT now knows that the whole file is a class 
+class_name GridItem 
+
+# extends from <Base Class>  # This is for inheritance
+
+var x = 0
+var y = 0
+
+# Init
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Basically Game loop
